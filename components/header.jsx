65022708093a1e9d49392bb40f607f2911b4ce2c
SyedAdnanRahmane <syedadnanrahman071@gmail.com>
2025-06-13 23:38:30 +0600

    v3

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="flex justify-end gap-4 p-4">
+      <SignedOut>
+        <SignInButton mode="modal" />
+        <SignUpButton mode="modal" />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
