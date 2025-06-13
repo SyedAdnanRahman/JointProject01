15c48f19a7b9c5242e1c688f360beaa5d3e8d05e
iftekharhasan2 <coolboy3656@gmail.com>
2025-06-13 23:24:21 +0600

    header

--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,25 @@
+import { headers } from 'next/headers'
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <nav>
+            <Link href="/">
+                <Image src="/logo.png" alt="Logo" width={200} height={60} className='h-12 py-1 w-auto object-contain' />
+            </Link>
+            <div className="">
+
+                
+
+            </div>
+
+        </nav>
+
+
+    </header>
   )
 }
 
