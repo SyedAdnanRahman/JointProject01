d8e1c60c9834d081eb7f16bf6f77e5849f5beeaf
SyedAdnanRahmane <syedadnanrahman071@gmail.com>
2025-06-15 18:38:50 +0600

    v15

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,62 +1,62 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Link } from "lucide-react";
-import { useEffect, useRef } from "react"
-import { Image } from "lucide-react";
-
- 
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useRef } from "react";
  
 
 const HeroSection = () => {
   const imageRef = useRef(null);
 
   useEffect(() => {
-    const imageElement = imageRef
+    const imageElement = imageRef.current;
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
       const scrollThreshold = 100;
+
       if (scrollPosition > scrollThreshold) {
-         imageElement.classList.add('scrolled');
+        imageElement.classList.add("scrolled");
       } else {
         imageElement.classList.remove("scrolled");
       }
-    }
+    };
+
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [])
+  }, []);
   
-    return(
+  return (
     <section className="w-full pt-36 md:pt-48 pb-10">
-
         <div className="space-y-6 text-center">
             <div className="space-y-6 mx-auto">
-                <h1 className="text-5xl font-bold md:text-6x1 lg:text-7x1 xl:text-8xl gradient-title">
-                    Your AI Career Coach for 
-                    <br/>
+          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title">
+            Your AI Career Coach for <br />
                     Professional Success
                 </h1>
                 <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
-                    advance your career with personalized guidence, interview prep, and AI-powered tools for job success.
+            Advance your career with personalized guidance, interview prep, and
+            AI-powered tools for job success.
                 </p>
             </div>
 
             <div className="flex justify-center space-x-4">
-                <Link href="/dashboard" >
+          <Link href="/dashboard">
                   <Button size="lg" className="px-8">
                     Get Started
                   </Button>
                 </Link>
-                 <Link href="https://youtu.be/bhdDznpJRkw?si=-mIsJBjdT-DFKYpj" >
+          <Link href="https://youtu.be/bhdDznpJRkw?si=-mIsJBjdT-DFKYpj" target="_blank">
                   <Button size="lg" className="px-8">
-                    surprize
+              Surprise
                   </Button>
                 </Link>
             </div>
-            <div className="hero-image-wrapper mt-5 md:mt-0 "> 
+
+        <div className="hero-image-wrapper mt-5 md:mt-0">
               <div ref={imageRef} className="hero-image">
                 <Image
-                src={"/banner.jpeg"}
+              src="/banner.jpeg"
                 width={1280}
                 height={720}
                 alt="Banner Sensai"
@@ -67,6 +67,7 @@ const HeroSection = () => {
             </div>
         </div>
     </section>
-    )
+  );
 };
-export default HeroSection
+
+export default HeroSection;
